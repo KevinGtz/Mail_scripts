839e37771d66a60b6445df1abf88bbb12a42f11c
Kevin Gutierrez <kevin@novelistik.com>
2017-05-07 23:25:43 -0500

    Created a new module where a async method is implemented in order tu put the sending mail process in a secundary thread

--- send_mails_with_async.py
+++ send_mails_with_async.py
@@ -19,13 +19,20 @@ app.config['FLASKY_ADMIN'] = the_reciper
 
 mail = Mail(app)
 
+
+def send_async_email(app, msg):
+    with app.app_context():
+        mail.send(msg)
+
+
 def send_email(to, subject, message):
     msg = Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + '' + subject,
     sender=app.config['FLASKY_MAIL_SENDER'], recipients=[to])
     msg.body = "Hello"
     msg.html = message
-    with app.app_context():
-        mail.send(msg)
+    thr = Thread(target=send_async_email, args=[app, msg])
+    thr.start()
+    return thr
 
 reciper = raw_input('Para quien es el mail? \n')
 subject = raw_input('Cual será el asunto? \n')
